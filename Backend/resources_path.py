ad548007b813640dcb83e1437dbec2044f8a351d
Adam Dolowy <adamdolowy2016@gmail.com>
2023-11-15 19:42:52 +0100

    Fixed error with quadruple backslash

--- Backend/resources_path.py
+++ Backend/resources_path.py
@@ -1,3 +1,3 @@
 #Function responsible for creating an absolute path to a file in resources
 def create_resources_file_path(rootPath, filename):
-    return f"{rootPath}\\\\resources\\\\{filename}"
+    return f"{rootPath}\\resources\\{filename}"

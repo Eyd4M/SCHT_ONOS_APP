bb714d76e83c5faa8fff9da77be1e34fb844f4d5
Adam Dolowy <adamdolowy2016@gmail.com>
2023-11-19 13:34:11 +0100

    Modified TCP taken Bandwidth calculations

--- Backend/bw_management.py
+++ Backend/bw_management.py
@@ -9,7 +9,6 @@ def update_link_bandwidth(graph, path, single_iperf, iperf_number, rtt):
         block_size = int(single_iperf.loc[iperf_number]['l'].replace('B', ''))
         # Calculating taken bandwidth considering an IP and UDP header size
         taken_bw = round(pps * (block_size+28) * 8 / 1000000, 2)
-        print(taken_bw)
 
         path_edges = list(zip(path, path[1:]))
         for edge in graph.edges():
@@ -18,14 +17,12 @@ def update_link_bandwidth(graph, path, single_iperf, iperf_number, rtt):
                 if curr_bw - taken_bw > 0:
                     graph[edge[0]][edge[1]]['bandwidth'] = round(curr_bw - taken_bw, 2)
                 else:
-                    graph[edge[0]][edge[1]]['bandwidth'] = 0.00000001
+                    graph[edge[0]][edge[1]]['bandwidth'] = 0.01
     # TCP stream
     else:
         drive_speed = int(single_iperf.loc[iperf_number]['b'].replace('m', ''))
-        #data_size = int(single_iperf.loc[iperf_number]['n'].replace('M', ''))
         tcp_window = int(single_iperf.loc[iperf_number]['w'].replace('K', ''))
-        taken_bw = min(round(((tcp_window * 8000) / (rtt/1000)) / 1000000, 2), drive_speed)
-        print(taken_bw)
+        taken_bw = min(round(((tcp_window * 2 * 8000) / (rtt/1000)) / 1000000, 2), drive_speed)
 
         path_edges = list(zip(path, path[1:]))
         for edge in graph.edges():
@@ -35,4 +32,3 @@ def update_link_bandwidth(graph, path, single_iperf, iperf_number, rtt):
                     graph[edge[0]][edge[1]]['bandwidth'] = round(curr_bw - taken_bw, 2)
                 else:
                     graph[edge[0]][edge[1]]['bandwidth'] = 0.00000001
-
